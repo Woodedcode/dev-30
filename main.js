0efe5d621535cf9732dd4c475c77f96805067b97
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-01-24 23:10:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -37,3 +37,39 @@ myAgeInDogYears = earlyYears + laterYears;
 var name = 'Ryan';
 console.log(`my name is ${name}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`)
 
+
+
+// // MAGIC 8 BALL
+//setting variable to empty string
+var userName = 'Ryan';
+
+userName ? console.log(`hello ${userName}`) : console.log('Hello')
+let userQuestion = 'Can I do a magic trick?';
+console.log(`${userName} asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+if (randomNumber === 0) {
+    eightBall = 'It is certain'
+} else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so'
+} else if (randomNumber === 2) {
+    eightBall = 'Reply hazy, try again'
+} else if (randomNumber === 3) {
+    eightBall = 'Cannot predict now'
+} else if (randomNumber === 4) {
+    eightBall = 'Do not count on it'
+} else if (randomNumber === 5) {
+    eightBall = 'My sources say no'
+} else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good'
+} else if (randomNumber === 7) {
+    eightBall = 'Signs point to yes'
+} else {
+    console.log('The 8 Ball is cracked')
+}
+
+console.log(eightBall)
+
+
+
