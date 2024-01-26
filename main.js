2868cbdcb2de21eb9ffa3e6a85a72d57488c0be3
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-01-25 22:02:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -91,3 +91,70 @@ if (registerEarly && runnerAge > 18){
 } else {
     console.log('PLEASE REGISTER')
 }
+
+
+// // ROCK, PAPER, SCISSORS EXERCISE
+
+    const getUserChoice = userInput => {
+       userInput = userInput.toLowerCase();
+        if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' || userInput === 'bomb') {
+            return userInput;
+        } else {
+            console.log('please select rock, paper, or scissors');
+        }
+    }
+
+const getComputerChoice = () => {
+     const randomNumber = Math.floor(Math.random() * 3)
+     switch (randomNumber) {
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissors';
+     }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'GAME IS A TIE'
+    }
+
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return "Sorry, computer won!"
+        } else {
+            return 'You won!'
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'Sorry, computer won'
+        } else {
+            return 'You won!'
+        }
+    }
+
+    if (userChoice === 'scissors'){
+        if (computerChoice === 'rock') {
+            return 'Sorry, computer won!'
+        } else {
+            console.log('You won!')
+        }
+    }
+
+    if (userChoice === 'bomb') {
+        return 'YOU WON!'
+    }
+}
+
+
+const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log(`you threw ${userChoice}`)
+    console.log(`computer threw ${computerChoice}`)
+
+    console.log(determineWinner(userChoice, computerChoice))
+}   
+
+playGame()
