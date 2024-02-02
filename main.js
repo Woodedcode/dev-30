6faaca592ba1ee89468d39852f9f7c3c91ea18b9
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-02-01 21:33:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -217,7 +217,6 @@ logTime(name2, days2);
 
 
 // // SECRET MESSAGE
-
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 console.log(secretMessage.pop())
 console.log(secretMessage.length)
