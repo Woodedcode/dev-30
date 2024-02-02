578c3a7c681ad41dc13786a5e79199d0d2c6a953
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-02-01 21:30:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -214,3 +214,19 @@ const name2 = 'Warren';
  
 logEvent(name2, event2);
 logTime(name2, days2);
+
+
+// // SECRET MESSAGE
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.pop())
+console.log(secretMessage.length)
+console.log(secretMessage.push('to','Program'))
+console.log(secretMessage)
+console.log(secretMessage.splice(7,1,'right'))
+console.log(secretMessage)
+console.log(secretMessage[0] = 'Programming')
+console.log(secretMessage)
+console.log(secretMessage.splice(6,5,'know,'))
+console.log(secretMessage)
+console.log(secretMessage.join())
