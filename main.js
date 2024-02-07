6d438def65649b941cd333336c1807b5484c04eb
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-02-06 21:38:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -229,3 +229,29 @@ console.log(secretMessage)
 console.log(secretMessage.splice(6,5,'know,'))
 console.log(secretMessage)
 console.log(secretMessage.join())
+
+
+// // WHALE TALK
+const input = 'turpentine and turtles';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    // console.log(input[i])
+    if (input[i] === 'u') {
+        resultArray.push(input[i])
+    }
+    if (input[i] === 'e') {
+        resultArray.push(input[i])
+    }
+    for (let j = 0; j < vowels.length; j++) {
+        // console.log(vowels[j])
+        if (vowels[j] === input[i] ) {
+            resultArray.push(input[i])
+        }
+    }
+}
+
+const resultString = resultArray.join(' ').toUpperCase();
+console.log(resultString)
